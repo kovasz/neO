b7aaf6aec061ad145906de4e8147394fa42d75ab
drkovasz@gmail.com <kovasznai.gergely@uni-eszterhazy.hu>
2020-07-29 18:12:19 +0200

    script to install the environment for starexec

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-pyinstaller --onefile --add-binary '/usr/local/lib64/python3.6/site-packages/z3/lib/libz3.so:.' --hidden-import="numpy.random.common" --hidden-import="numpy.random.bounded_integers" --hidden-import="numpy.random.entropy" neO.py
---
+++ scripts/install_for_starexec.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+yum update
+yum -y install centos-release-scl
+yum -y install epel-release
+
+# yum install rh-python38 rh-python38-python-devel
+# scl enable rh-python38 bash
+# pip3 install numpy parse wheel pathos python-sat pysmt
+
+yum -y install python36 python36-devel python36-pip
+
+pip3 install setuptools numpy parse wheel pathos python-sat
+
+pip3 install -Iv pysmt==0.8.0
+pysmt-install --confirm-agreement --z3
+pysmt-install --confirm-agreement --yices
+
+pip3 install pyinstaller
