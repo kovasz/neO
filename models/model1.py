0ceb569cee8cd1245491ebb061a47a6b3f64a155
drkovasz@gmail.com <kovasznai.gergely@uni-eszterhazy.hu>
2020-08-12 09:51:21 +0200

    missing files: models and mip solver added

---
+++ models/model1.py
@@ -0,0 +1,171 @@
+# -*- coding: utf-8 -*-
+
+from parse import search
+from math import pow, sqrt, ceil
+import logging
+
+from solvers.card_enc_type import Relations
+from solvers.solver import Constraint
+
+from models.model import WsnModel
+
+class Sensor:
+	powerConsumptions = {
+		120: 17.4,
+		109: 16.5,
+		92: 15.2,
+		75: 13.9,
+		58: 12.5,
+		41: 11.2,
+		25: 9.9,
+		7: 8.5
+	}
+
+	maxEnergy = 500
+
+	def __init__(self, x, y, scope):
+		self.x = x
+		self.y = y
+		self.scope = scope
+		self.lifetime = int(Sensor.maxEnergy / Sensor.powerConsumptions[scope])
+
+	def __str__(self):
+		return "({:d},{:d}): scope = {:d}, lifetime = {:d}".format(self.x, self.y, self.scope, self.lifetime)
+
+class Point:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+		self.converingSensorIndices = []
+	
+	def DistanceFrom(self, sensor):
+		return sqrt(pow(sensor.x - self.x, 2) + pow(sensor.y - self.y, 2))
+
+	def SetCoverage(self, sensors):
+		for i in range(len(sensors)):
+			if self.DistanceFrom(sensors[i]) <= sensors[i].scope:
+				self.converingSensorIndices.append(i)
+
+	def __str__(self):
+		return "({:d},{:d}): covering sensors = {}".format(int(self.x), int(self.y), self.converingSensorIndices)
+
+class WsnModel1(WsnModel):
+	def __init__(self, limit_covering, limit_ON, limit_crit_ON):
+		super().__init__(limit_covering, limit_ON, limit_crit_ON)
+
+	def ReadInputFile(self, filename):
+		with open(filename) as inputFile:
+			for line in inputFile:
+				r = search("{}_sensor: x = {x:f} y = {y:f} scope = {scope:d}", line)
+				if r is not None:
+					self.sensors.append(Sensor(int(r["x"]), int(r["y"]), r["scope"]))
+				else:
+					r = search("{}_point: x = {x:d} y = {y:d}", line)
+					if r is not None:
+						self.points.append(Point(r["x"], r["y"]))
+		
+		for target in self.points:
+			target.SetCoverage(self.sensors)
+
+		# let the 1st half of points be marked as "critical"
+		for i in range(0, int(len(self.points) / 2)):
+			self.critical_points.append(self.points[i])
+
+	def __SensorCoversCriticalPoint(self, sensorIndex):
+		for p in self.critical_points:
+			if sensorIndex in p.converingSensorIndices:
+				return True
+		return False
+
+	def GetUpperBound(self):
+		return ceil(sum(s.lifetime for s in self.sensors) / self.limit_covering)
+
+	def GetResource(self, schedulingModel):
+		return sum(s.lifetime for s in self.sensors) - sum(1 if lit > 0 else 0 for l in schedulingModel for lit in l)
+
+	def EncodeWsnConstraints(self, lifetime, solver):
+		# generate scheduling vars
+		schedulingVars = [solver.generateVars(lifetime) for _ in self.sensors]
+
+		# lifetime constraint
+		for sensorIndex in range(len(self.sensors)):
+			solver.addConstraint(Constraint(
+				lits = schedulingVars[sensorIndex],
+				relation = Relations.LessOrEqual,
+				bound = self.sensors[sensorIndex].lifetime
+			))
+
+		# coverage constraint
+		for point in self.points:
+			for time in range(lifetime):
+				solver.addConstraint(Constraint(
+					lits = [schedulingVars[sensorIndex][time] for sensorIndex in point.converingSensorIndices],
+					relation = Relations.GreaterOrEqual,
+					bound = self.limit_covering
+				))
+
+		# evasive constraint
+		if self.limit_ON > 0:
+			for sensorIndex in range(len(self.sensors)):
+				for time in range(lifetime - self.limit_ON):
+					solver.addConstraint(Constraint(
+						lits = [schedulingVars[sensorIndex][time + h] for h in range(self.limit_ON + 1)],
+						relation = Relations.LessOrEqual,
+						bound = self.limit_ON
+					))
+
+		# moving target constraint
+		if self.limit_crit_ON > 0:
+			for sensorIndex in range(len(self.sensors)):
+				if not self.__SensorCoversCriticalPoint(sensorIndex):
+					continue
+
+				for time in range(lifetime - self.limit_crit_ON):
+					solver.addConstraint(Constraint(
+						lits = [schedulingVars[sensorIndex][time + h] for h in range(self.limit_crit_ON + 1)],
+						relation = Relations.LessOrEqual,
+						bound = self.limit_crit_ON
+					))
+
+		return schedulingVars
+
+	def VerifyScheduling(self, schedulingModel, lifetime):
+		# lifetime constraint
+		for sensorIndex in range(len(self.sensors)):
+			s = sum(1 for x in schedulingModel[sensorIndex] if x > 0)
+			logging.debug("Verify the lifetime constraint for sensor #{:d}:\t{:d} <= {:d}".format(sensorIndex, s, self.sensors[sensorIndex].lifetime))
+			assert s <= self.sensors[sensorIndex].lifetime, "Verification failed: lifetime constraint violated for sensor #{:d}".format(sensorIndex)
+
+		# coverage constraint
+		for pointIndex in range(len(self.points)):
+			for time in range(lifetime):
+				s = sum(1 for sensorIndex in self.points[pointIndex].converingSensorIndices if schedulingModel[sensorIndex][time] > 0)
+				logging.debug("Verify the coverage constraint for point #{:d} and time {:d}:\t{:d} >= {:d}".format(pointIndex, time, s, self.limit_covering))
+				assert s >= self.limit_covering, "Verification failed: coverage constraint violated for point #{:d} and time {:d}".format(pointIndex, time)
+
+		# evasive constraint
+		if self.limit_ON > 0:
+			for sensorIndex in range(len(self.sensors)):
+				for time in range(lifetime - self.limit_ON):
+					s = sum(1 for h in range(self.limit_ON + 1) if schedulingModel[sensorIndex][time + h] > 0)
+					logging.debug("Verify the evasive constraint for sensor {:d} and time {:d}:\t{:d} <= {:d}".format(sensorIndex, time, s, self.limit_ON))
+					assert s <= self.limit_ON, "Verification failed: evasive constraint violated for sensor {:d} and time {:d}".format(sensorIndex, time)
+
+		# moving target constraint
+		if self.limit_crit_ON > 0:
+			for sensorIndex in range(len(self.sensors)):
+				if not self.__SensorCoversCriticalPoint(sensorIndex):
+					continue
+
+				for time in range(lifetime - self.limit_crit_ON):
+					s = sum(1 for h in range(self.limit_crit_ON + 1) if schedulingModel[sensorIndex][time + h] > 0)
+					logging.debug("Verify the moving target constraint for sensor {:d} and time {:d}:\t{:d} <= {:d}".format(sensorIndex, time, s, self.limit_crit_ON))
+					assert s <= self.limit_crit_ON, "Verification failed: moving target constraint violated for sensor {:d} and time {:d}".format(sensorIndex, time)
+
+	def DisplayScheduling(self, schedulingModel):
+		for sensor in range(len(schedulingModel)):
+			print("Sensor #{:d}:\t".format(sensor), end = "")
+			for time in range(len(schedulingModel[sensor])):
+				print("{:d}\t".format(time + 1) if schedulingModel[sensor][time] > 0 else "\t", end = "")
+			print()
+
---
+++ models/model2.py
@@ -0,0 +1,217 @@
+# -*- coding: utf-8 -*-
+
+from parse import search
+from math import pow, sqrt, ceil
+import logging
+
+from solvers.card_enc_type import Relations
+from solvers.solver import Constraint
+
+from models.model import WsnModel
+
+class Sensor:
+	def __init__(self, x, y, power):
+		self.x = x
+		self.y = y
+		self.fullPower = power
+
+	def __str__(self):
+		return "({:d},{:d}): full power = {:d}".format(self.x, self.y, self.fullPower)
+
+class Point:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+
+	def DistanceFrom(self, sensor):
+		return sqrt(pow(sensor.x - self.x, 2) + pow(sensor.y - self.y, 2))
+
+	def __str__(self):
+		return "({:d},{:d})".format(self.x, self.y)
+
+class Level:
+	def __init__(self, power, range):
+		self.power = power
+		self.range = range
+	
+	def __str__(self):
+		return "(power = {:d}, range = {:d})".format(self.power, self.range)
+
+class WsnModel2(WsnModel):
+	def __init__(self, limit_covering, limit_ON, limit_crit_ON):
+		super().__init__(limit_covering, limit_ON, limit_crit_ON)
+		self.levels = []
+
+	def ReadInputFile(self, filename):
+		with open(filename) as inputFile:
+			for line in inputFile:
+				r = search("{}_sensor: x = {x:d} y = {y:d} power = {power:d}", line)
+				if r is not None:
+					self.sensors.append(Sensor(r["x"], r["y"], r["power"]))
+				else:
+					r = search("{}_point: x = {x:d} y = {y:d}", line)
+					if r is not None:
+						self.points.append(Point(r["x"], r["y"]))
+					else:
+						r = search("{}_level: power = {power:d} range = {range:d}", line)
+						if r is not None:
+							self.levels.append(Level(r["power"], r["range"]))
+		
+		if self.sensors is None or self.points is None or self.levels is None:
+			raise Exception("No sensors or target points or performance levels specified")
+
+		# let the 1st half of points be marked as "critical"
+		for i in range(0, int(len(self.points) / 2)):
+			self.critical_points.append(self.points[i])
+		
+		self.levels = sorted(self.levels, key = lambda level: level.power)
+
+	def GetUpperBound(self):
+		return ceil(sum(ceil(s.fullPower / self.levels[0].power) for s in self.sensors) / self.limit_covering)
+
+	def GetResource(self, schedulingModel):
+		s = 0
+		for sensorIndex in range(len(schedulingModel)):
+			for time in range(len(schedulingModel[sensorIndex])):
+				try:
+					s += next(self.levels[i].power for i in range(len(schedulingModel[sensorIndex][time])) if schedulingModel[sensorIndex][time][i] > 0)
+				except:
+					pass
+					
+		return sum(s.fullPower for s in self.sensors) - s
+
+	def EncodeWsnConstraints(self, lifetime, solver):
+		# generate and constraint scheduling vars (at most 1 scheduling var per sensor and time interval may be true)
+		schedulingVars = [[solver.generateVars(len(self.levels)) for _ in range(lifetime)] for _ in self.sensors]
+
+		for sensorIndex in range(len(self.sensors)):
+			for time in range(lifetime):
+				solver.addConstraint(Constraint(
+					lits = schedulingVars[sensorIndex][time],
+					relation = Relations.LessOrEqual,
+					bound = 1
+				))
+		# there exists a pure Boolean encoding, as well
+
+		# lifetime constraint
+		for sensorIndex in range(len(self.sensors)):
+			lits = []
+			weights = []
+			for time in range(lifetime):
+				lits.extend(schedulingVars[sensorIndex][time])
+				weights.extend([level.power for level in self.levels])
+			solver.addConstraint(Constraint(
+				lits = lits,
+				weights = weights,
+				relation = Relations.LessOrEqual,
+				bound = self.sensors[sensorIndex].fullPower
+			))
+
+		# coverage and define vars
+		coverageVars = [[solver.generateVars(lifetime) for _ in self.points] for _ in self.sensors]
+		for sensorIndex in range(len(self.sensors)):
+			for pointIndex in range(len(self.points)):
+				distance = ceil(self.points[pointIndex].DistanceFrom(self.sensors[sensorIndex]))
+				for time in range(lifetime):
+					solver.addConstraint(Constraint(
+						lits = schedulingVars[sensorIndex][time],
+						weights = [level.range for level in self.levels],
+						relation = Relations.GreaterOrEqual,
+						bound = distance,
+						boolLit = coverageVars[sensorIndex][pointIndex][time]
+					))
+
+		# coverage constraint
+		for pointIndex in range(len(self.points)):
+			for time in range(lifetime):
+				solver.addConstraint(Constraint(
+					lits = [coverageVars[sensorIndex][pointIndex][time] for sensorIndex in range(len(self.sensors))],
+					relation = Relations.GreaterOrEqual,
+					bound = self.limit_covering
+				))
+
+		# evasive constraint
+		if self.limit_ON > 0:
+			for sensorIndex in range(len(self.sensors)):
+				for time in range(lifetime - self.limit_ON):
+					lits = []
+					for h in range(self.limit_ON + 1):
+						lits.extend(schedulingVars[sensorIndex][time + h])
+					solver.addConstraint(Constraint(
+						lits = lits,
+						relation = Relations.LessOrEqual,
+						bound = self.limit_ON
+					))
+		# there exists a pure Boolean encoding, as well
+
+		# moving target constraint
+		if self.limit_crit_ON > 0:
+			for sensorIndex in range(len(self.sensors)):
+				for pointIndex in range(len(self.critical_points)):
+					for time in range(lifetime - self.limit_crit_ON):
+						solver.addClause([-coverageVars[sensorIndex][pointIndex][time + h] for h in range(self.limit_crit_ON + 1)])
+
+		return schedulingVars
+
+	def VerifyScheduling(self, schedulingModel, lifetime):
+		# lifetime constraint
+		for sensorIndex in range(len(self.sensors)):
+			s = 0
+			for time in range(lifetime):
+				try:
+					s += next(self.levels[i].power for i in range(len(schedulingModel[sensorIndex][time])) if schedulingModel[sensorIndex][time][i] > 0)
+				except:
+					pass
+			logging.debug("Verify the lifetime constraint for sensor #{:d}:\t{:d} <= {:d}".format(sensorIndex, s, self.sensors[sensorIndex].fullPower))
+			assert s <= self.sensors[sensorIndex].fullPower, "Verification failed: lifetime constraint violated for sensor #{:d}".format(sensorIndex)
+
+		# coverage constraint
+		for pointIndex in range(len(self.points)):
+			for time in range(lifetime):
+				s = 0
+				for sensorIndex in range(len(self.sensors)):
+					try:
+						r = next(self.levels[i].range for i in range(len(schedulingModel[sensorIndex][time])) if schedulingModel[sensorIndex][time][i] > 0)
+						if r >= self.points[pointIndex].DistanceFrom(self.sensors[sensorIndex]):
+							s += 1
+					except:
+						pass
+
+				logging.debug("Verify the coverage constraint for point #{:d} and time {:d}:\t{:d} >= {:d}".format(pointIndex, time, s, self.limit_covering))
+				assert s >= self.limit_covering, "Verification failed: coverage constraint violated for point #{:d} and time {:d}".format(pointIndex, time)
+
+		# evasive constraint
+		if self.limit_ON > 0:
+			for sensorIndex in range(len(self.sensors)):
+				for time in range(lifetime - self.limit_ON):
+					s = sum(1 for h in range(self.limit_ON + 1) for i in range(len(schedulingModel[sensorIndex][time])) if schedulingModel[sensorIndex][time + h][i] > 0)
+					logging.debug("Verify the evasive constraint for sensor {:d} and time {:d}:\t{:d} <= {:d}".format(sensorIndex, time, s, self.limit_ON))
+					assert s <= self.limit_ON, "Verification failed: evasive constraint violated for sensor {:d} and time {:d}".format(sensorIndex, time)
+
+		# moving target constraint
+		if self.limit_crit_ON > 0:
+			for sensorIndex in range(len(self.sensors)):
+				for pointIndex in range(len(self.critical_points)):
+					for time in range(lifetime - self.limit_crit_ON):
+						s = 0
+						for h in range(self.limit_crit_ON + 1):
+							try:
+								r = next(self.levels[i].range for i in range(len(schedulingModel[sensorIndex][time + h])) if schedulingModel[sensorIndex][time + h][i] > 0)
+								if r >= self.points[pointIndex].DistanceFrom(self.sensors[sensorIndex]):
+									s += 1
+							except:
+								pass
+
+						logging.debug("Verify the moving target constraint for sensor {:d} and time {:d}:\t{:d} <= {:d}".format(sensorIndex, time, s, self.limit_crit_ON))
+						assert s <= self.limit_crit_ON, "Verification failed: moving target constraint violated for sensor {:d} and time {:d}".format(sensorIndex, time)
+
+	def DisplayScheduling(self, schedulingModel):
+		for sensor in range(len(schedulingModel)):
+			print("Sensor #{:d}:\t".format(sensor), end = "")
+			for time in range(len(schedulingModel[sensor])):
+				try:
+					print("{:d}->{:d}\t".format(time, next(i + 1 for i in range(len(schedulingModel[sensor][time])) if schedulingModel[sensor][time][i] > 0)), end = "")
+				except StopIteration:
+					print("\t", end = "")
+			print()
+
